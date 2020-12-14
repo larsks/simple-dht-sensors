c769c737dd1a40741655d8fb41d9904195435e51
Lars Kellogg-Stedman <lars@oddbit.com>
2020-12-14 17:47:42 -0500

    we now have multiple configs

--- sensor/config_bedroom.py
+++ sensor/config_bedroom.py
@@ -1,9 +1,9 @@
 mqtt_server = '192.168.1.200'
 dht_pin = 4
 led_pin = 2
-blink_led = True
 
 # 30 seconds
 loop_interval = 30000
 
-location = 'office'
+location = 'master_bedroom'
+blink_led = False
