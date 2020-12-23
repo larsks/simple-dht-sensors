3cc491083316858d9f3fec855341f7b46a6b0fff
Lars Kellogg-Stedman <lars@oddbit.com>
2020-12-23 10:05:32 -0500

    add a bunch of configs

---
+++ sensor/config_dev.py
@@ -0,0 +1,4 @@
+mqtt_server = '192.168.1.200'
+location = 'testing'
+blink_led = True
+measure_interval = 60000
--- sensor/config_office.py
+++ sensor/config_office.py
@@ -1,4 +1,4 @@
 mqtt_server = '192.168.1.200'
-location = 'master_bedroom'
+location = 'office'
 blink_led = True
 measure_interval = 300000
